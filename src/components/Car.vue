861cbe27d23d9fbe779fc40a8cbc6cb1a55d2640
Harrison Wheeler <wheelerharrison93@gmail.com>
2021-03-16 18:04:44 -0600

    fireside with modals and props

---
+++ src/components/Car.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-4">
+    <!-- NOTE the route contains both the name and an object to provide needed params -->
+      <div class="card car">
+        <img class="card-img-top" :src="car.imgUrl" alt="car">
+        <div class="card-body">
+          <h4 class="card-title">{{car.make}} | {{car.model}}</h4>
+          <p class="card-text">{{car.year}} | {{car.price}}</p>
+        </div>
+      </div>
+        <button class="btn btn-primary" data-toggle="modal" :data-target="'#carDetailsModal' + car._id">Car Deetz</button>
+      <CarDetailsModal :carBanana="car" :testString="'hi isabelle!'" />
+  </div>
+</template>
+
+<script>
+import CarDetailsModal from '../components/CarDetailsModal'
+
+export default {
+  name: 'car',
+  props: {
+    car: Object
+  },
+  setup() {
+    return {}
+  },
+  components: {
+    CarDetailsModal
+  }
+}
+</script>
+
+<style scoped>
+.car:hover {
+  transform: translateY(-5px);
+  box-shadow: 1px 3px 5px rgb(94, 94, 94);
+  cursor: pointer
+}
+
+.car {
+  transition: all .2s;
+  color: black
+}
+</style>
---
+++ src/components/CarDetailsModal.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="modal carDetailsModal" :id="'carDetailsModal' + carBanana._id" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Make: {{carBanana.make}}</p>
+        <p>Model: {{carBanana.model}}</p>
+        <p>Year: {{carBanana.year}}</p>
+        <p>Price: {{carBanana.price}}</p>
+        <p></p>
+        <p>{{testString}}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" @click="deleteCar()">Delort</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import { carsService } from '../services/CarsService'
+// import $ from 'jquery'
+// NOTE if using npm to install jquery as a library - otherwise make sure script tags are in the index.html
+
+export default {
+  name: 'CarDetailsModal',
+  props: {
+    carBanana: { type: Object, required: true },
+    testString: { type: String, required: true }
+  },
+  setup(props) {
+    return {
+      async deleteCar() {
+        await carsService.deleteCar(props.carBanana._id)
+        // eslint-disable-next-line no-undef
+        $('#carDetailsModal' + `${props.carBanana._id}`).modal('toggle')
+      }
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
